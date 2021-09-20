48b3d08b52dbaaddf85fc22bff3f5d647d885cac
Stefan Frauli <frauli@web.de>
2021-09-20 08:52:41 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Stefan Frauli on 20.09.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
